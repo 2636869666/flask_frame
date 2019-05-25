5a5efa178beb91ad04c5b78834924b7743f0326a
kaige <2636869666@qq.com>
2019-05-25 09:30:23 +0800

    '首页'

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'this is index html'
+    return render_template('index.html')
+
+@app.route('/login',methods=["get","post"])
+def login():
+    if request.method == 'post':
+        uname = request.form.get("username")
+        pwd = request.form.get("password")
+        if uname == 'a' and pwd == 'a':
+            response = make_response("登录成功")
+
+            return response
+    return render_template('login.html')
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
